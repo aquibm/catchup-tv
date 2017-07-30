9cfb5d138e35995c132974e1af5c6baae2b64e2b
Aquib Master <aquibmaster@gmail.com>
2017-07-30 15:38:44 +1200

    Spike: Handle search, fetch shows and render them

--- screens/search/Search.js
+++ screens/search/Search.js
@@ -1,19 +1,72 @@
 import React, { Component } from 'react'
-import { Text, View, Button, StyleSheet } from 'react-native'
+import { View, TextInput, Text, Image } from 'react-native'
 
 import navigatableScreen from '../navigatableScreen'
 
 class SearchScreen extends Component {
-    goToShow = () => {
+    state = {
+        searchText: 'Game of thrones',
+    }
+
+    _goToShow = () => {
         const { navigate } = this.props.navigation
         navigate('Show')
     }
 
+    _handleSearchChange = searchText => {
+        this.setState(state => ({
+            ...state,
+            searchText,
+        }))
+    }
+
+    _search = async () => {
+        const { searchText } = this.state
+
+        if (!searchText || searchText.length < 3) return // TODO(AM): Alert
+
+        response = await fetch(
+            `http://api.tvmaze.com/search/shows?q=${searchText}`,
+        )
+
+        json = await response.json()
+
+        this.setState(state => ({
+            ...state,
+            response: json,
+        }))
+    }
+
+    renderShow(show) {
+        return (
+            <View key={show.id}>
+                <Image
+                    source={{ uri: show.image.medium }}
+                    style={{ width: 210, height: 295 }}
+                />
+                <Text>
+                    {show.name}
+                </Text>
+            </View>
+        )
+    }
+
     render() {
+        const { searchText, response } = this.state
+
         return (
             <View>
-                <Text>Search Screen!</Text>
-                <Button title="Go to show!" onPress={this.goToShow} />
+                <TextInput
+                    value={searchText}
+                    onChangeText={this._handleSearchChange}
+                    onSubmitEditing={this._search}
+                    style={{ height: 44, padding: 8 }}
+                    autoCapitalize="sentences"
+                    autoFocus={true}
+                    placeholder="Search for TV shows"
+                />
+                {response &&
+                    response.map(result => this.renderShow(result.show))}
             </View>
         )
     }
